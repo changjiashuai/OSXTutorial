2c0e518518d5e76a207f80b0b7c92d88bb54a44f
changjiashuai <changjiashuai@gmail.com>
2015-10-14 22:17:59 +0800

    Initial Commit

---
+++ OSXTutorial/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OSXTutorial
+//
+//  Created by CJS on 15/10/14.
+//  Copyright © 2015年 CJS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
